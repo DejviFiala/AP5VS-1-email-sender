8e53594226840199df298b8f765d736ff10bea3b
David Fiala <david.fiala.44@gmail.com>
2024-09-30 18:45:39 +0200

    Finished task

--- app/src/main/java/utb/fai/App.java
+++ app/src/main/java/utb/fai/App.java
@@ -3,11 +3,23 @@ package utb.fai;
 public class App {
 
     public static void main(String[] args) {
-        // TODO: Implement input parameter processing
-        
+        // Kontrola počtu argumentů
+        if (args.length != 6) {
+            System.out.println("Použití: java App <host> <port> <from> <to> <subject> <text>");
+            return;
+        }
+
+        // Zpracování argumentů
+        String host = args[0];
+        int port = Integer.parseInt(args[1]);
+        String from = args[2];
+        String to = args[3];
+        String subject = args[4];
+        String text = args[5];
+
         try {
-            EmailSender sender = new EmailSender("smtp.utb.cz", 25);
-            sender.send("you@utb.cz", "you@utb.cz", "Email from Java", "Funguje to?\nSnad...");
+            EmailSender sender = new EmailSender(host, port);
+            sender.send(from, to, subject, text);
             sender.close();
         } catch (Exception e) {
             e.printStackTrace();
--- app/src/main/java/utb/fai/EmailSender.java
+++ app/src/main/java/utb/fai/EmailSender.java
@@ -4,13 +4,24 @@ import java.net.*;
 import java.io.*;
 
 public class EmailSender {
+    private Socket socket;
+    private BufferedReader reader;
+    private BufferedWriter writer;
     /*
      * Constructor opens Socket to host/port. If the Socket throws an exception
      * during opening,
      * the exception is not handled in the constructor.
      */
     public EmailSender(String host, int port) throws UnknownHostException, IOException {
+        socket = new Socket(host, port);
 
+        reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+        String response = reader.readLine();
+        System.out.println("Server: " + response);
+
+        sendCommand("EHLO localhost");
     }
 
     /*
@@ -20,13 +31,41 @@ public class EmailSender {
      * handled by this method.
      */
     public void send(String from, String to, String subject, String text) throws IOException {
-
+        sendCommand("MAIL FROM:<" + from + ">");
+        
+        sendCommand("RCPT TO:<" + to + ">");
+        
+        sendCommand("DATA");
+        
+        writer.write("From: " + from + "\r\n");
+        writer.write("To: " + to + "\r\n");
+        writer.write("Subject: " + subject + "\r\n");
+        writer.write("\r\n"); // Prázdný řádek mezi hlavičkami a tělem zprávy
+        writer.write(text + "\r\n");
+        writer.write(".\r\n"); // Označení konce zprávy
+        writer.flush();
+    
+        String response = reader.readLine();
+        System.out.println("Server: " + response);
     }
 
     /*
      * Sends QUIT and closes the socket
      */
     public void close() {
+        try {
+            sendCommand("QUIT");
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    private void sendCommand(String command) throws IOException {
+        writer.write(command + "\r\n");
+        writer.flush();
+    
+        String response = reader.readLine();
+        System.out.println("Server: " + response);
     }
 }
